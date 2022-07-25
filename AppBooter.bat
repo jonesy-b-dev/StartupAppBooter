60979db8b9ce6c1fb8dbc09956b4a1c7bafbeb3d
Jonas de Bruin <jonasdebruin05@gmail.com>
2022-07-25 11:31:07 +0200

    Run from py file

--- AppBooter.bat
+++
@@ -1,5 +0,0 @@
-cd c:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE
-start devenv.exe
-
-cd c:\Users\jonas\AppData\Local\Discord\app-1.0.9005
-start Discord.exe
--- AppBooter.py
+++ AppBooter.py
@@ -1,3 +1,2 @@
 import os
-os.system("cd C:/Users/jonas/AppData/Local/Discord/app-1.0.9005")
-os.system("start Discord.exe")
+os.system("start AppBooter")
