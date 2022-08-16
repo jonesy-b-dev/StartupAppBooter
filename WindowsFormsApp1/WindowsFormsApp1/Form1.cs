503653829f63c508a72a727e2fca75f103b7fd3f
Jonas de Bruin <jonasdebruin05@gmail.com>
2022-08-17 00:12:40 +0200

    Now uses C# form instead of python
    I'm going to use C# forms now to make the UI and run the bat file to make the app. No more Python

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("notepad will start now");
+            Process ExternalProcess = new Process();
+            ExternalProcess.StartInfo.FileName = "D:/Code Projects/Python/StartupAppBooter/AppBooter";
+            ExternalProcess.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+            ExternalProcess.Start();
+            ExternalProcess.WaitForExit();
+        }
+    }
+}
